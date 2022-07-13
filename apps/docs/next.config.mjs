c385650b7bfa9516fe7a95b7206b0b5e6fd8e034
Axel Lavoie <axel.lavoie@gmail.com>
2022-07-13 13:58:46 +0200

    Update route

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -6,15 +6,6 @@ import createMDX from '@next/mdx'
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/introduction',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 const withMDX = createMDX({
