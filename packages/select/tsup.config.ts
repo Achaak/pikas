4af557cb8ac921889c1c6bc7100afd85a88dd41e
Axel Lavoie <axel.lavoie@gmail.com>
2022-09-22 01:12:29 +0200

    fix: clean

--- packages/select/tsup.config.ts
+++ packages/select/tsup.config.ts
@@ -6,7 +6,6 @@ export default defineConfig((options) => {
     entry: ['src'],
     splitting: true,
     sourcemap: true,
-    clean: true,
     dts: true,
     external: ['react', 'react-dom'],
     format: ['cjs', 'esm'],
