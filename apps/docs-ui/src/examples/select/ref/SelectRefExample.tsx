9c6f00067f100bf635537252991468d4d210431d
Axel Lavoie <axel.lavoie@gmail.com>
2022-10-07 20:50:15 +0200

    feat: docs

---
+++ apps/docs-ui/src/examples/select/ref/SelectRefExample.tsx
@@ -0,0 +1,46 @@
+import { ExampleContainer } from '@pikas/docs-ui'
+import type { SelectRef } from '@pikas-ui/select'
+import { Select } from '@pikas-ui/select'
+import { useRef } from 'react'
+import { Button } from '@pikas-ui/button'
+
+export const SelectRefExample: React.FC = () => {
+  const selectRef = useRef<SelectRef>(null)
+
+  return (
+    <ExampleContainer>
+      <Select
+        ref={selectRef}
+        defaultValue="option-1"
+        data={[
+          {
+            items: [
+              {
+                label: 'Option 1',
+                value: 'option-1',
+              },
+              {
+                label: 'Option 2',
+                value: 'option-2',
+              },
+            ],
+          },
+        ]}
+      />
+      <Button
+        onClick={(): void => {
+          selectRef.current?.setValue('option-1')
+        }}
+      >
+        Set Option 1
+      </Button>
+      <Button
+        onClick={(): void => {
+          selectRef.current?.setValue('option-2')
+        }}
+      >
+        Set Option 2
+      </Button>
+    </ExampleContainer>
+  )
+}
