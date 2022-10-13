f0b23659d18317201b37548264405cf08fe0dbf6
Axel Lavoie <axel.lavoie@gmail.com>
2022-10-14 00:01:46 +0200

    feat(styles): test createPikas

--- packages/ui/styles/src/css.ts
+++ packages/ui/styles/src/css.ts
@@ -1,3 +1,4 @@
+import type { DefaultThemeMap } from '@stitches/react'
 import { createStitches } from '@stitches/react'
 
 import { BR } from './borderRadius.js'
@@ -15,43 +16,86 @@ import { Sizes } from './sizes.js'
 import { Space } from './space.js'
 import { Transitions } from './transitions.js'
 import { ZIndices } from './zIndices.js'
-import type * as Stitches from '@stitches/react'
+import type * as AllStitches from '@stitches/react'
+import type { ConfigType } from '@stitches/react/types/config.js'
+import type Stitches from '@stitches/react/types/stitches.js'
 
-export const stitchesConfig = createStitches({
-  prefix: '',
-  theme: {
-    colors: Colors,
-    fonts: {
-      roboto: 'Roboto',
+/*
+
+<
+		Prefix extends string = '',
+		Media extends {} = {},
+		Theme extends {} = {},
+		ThemeMap extends {} = DefaultThemeMap,
+		Utils extends {} = {}
+	>(
+		config?: {
+			prefix?: ConfigType.Prefix<Prefix>
+			media?: ConfigType.Media<Media>
+			theme?: ConfigType.Theme<Theme>
+			themeMap?: ConfigType.ThemeMap<ThemeMap>
+			utils?: ConfigType.Utils<Utils>
+		}
+	): Stitches<Prefix, Media, Theme, ThemeMap, Utils>
+}
+
+*/
+
+export const createPikas = <
+  Prefix extends string = '',
+  Media extends Record<string, unknown> = Record<string, unknown> & {
+    xs?: string
+    sm?: string
+    md?: string
+    lg?: string
+  },
+  Theme extends Record<string, unknown> = Record<string, unknown>,
+  ThemeMap extends DefaultThemeMap = DefaultThemeMap,
+  Utils extends Record<string, unknown> = Record<string, unknown>
+>(config?: {
+  prefix?: ConfigType.Prefix<Prefix>
+  media?: ConfigType.Media<Media>
+  theme?: ConfigType.Theme<Theme>
+  themeMap?: ConfigType.ThemeMap<ThemeMap>
+  utils?: ConfigType.Utils<Utils>
+}): Stitches<Prefix | 'pikas', Media, Theme, ThemeMap, Utils> =>
+  createStitches({
+    prefix: config?.prefix ?? 'pikas',
+    theme: {
+      colors: Colors,
+      fonts: {
+        roboto: 'Roboto',
+      },
+      shadows: Shadows,
+      fontSizes: FontSizes,
+      space: Space,
+      sizes: Sizes,
+      fontWeights: FontWeights,
+      lineHeights: LineHeights,
+      letterSpacings: LetterSpacings,
+      borderWidths: BorderWidths,
+      borderStyles: BorderStyles,
+      radii: Radii,
+      zIndices: ZIndices,
+      transitions: Transitions,
     },
-    shadows: Shadows,
-    fontSizes: FontSizes,
-    space: Space,
-    sizes: Sizes,
-    fontWeights: FontWeights,
-    lineHeights: LineHeights,
-    letterSpacings: LetterSpacings,
-    borderWidths: BorderWidths,
-    borderStyles: BorderStyles,
-    radii: Radii,
-    zIndices: ZIndices,
-    transitions: Transitions,
-  },
-  media: {
-    xs: '(min-width: 480px)',
-    sm: '(min-width: 640px)',
-    md: '(min-width: 768px)',
-    lg: '(min-width: 1024px)',
-    xl: '(min-width: 1280px)',
-  },
-  utils: {
-    ...gap,
-    ...BR,
-    linearGradient: (value: number | string) => ({
-      backgroundImage: `linear-gradient(${value})`,
-    }),
-  },
-})
+    media: {
+      xs: `(min-width: ${config?.media?.xs || 480}px)`,
+      sm: `(min-width: ${config?.media?.sm || 640}px)`,
+      md: `(min-width: ${config?.media?.md || 768}px)`,
+      lg: `(min-width: ${config?.media?.lg || 1024}px)`,
+      xl: `(min-width: ${config?.media?.xl || 1280}px)`,
+      ...config?.media,
+    },
+    utils: {
+      ...gap,
+      ...BR,
+      linearGradient: (value: number | string) => ({
+        backgroundImage: `linear-gradient(${value})`,
+      }),
+      ...config?.utils,
+    },
+  })
 
 export const {
   styled,
@@ -64,6 +108,18 @@ export const {
   prefix,
   reset,
   theme,
-} = stitchesConfig
+} = createPikas({
+  media: {
+    xxl: '(min-width: 1536px)',
+  },
+  utils: {
+    mar: (value: number | string) => ({
+      margin: value,
+    }),
+  },
+})
 
-export type CSS = Stitches.CSS<typeof config>
+type CSS = AllStitches.CSS<typeof config>
+const t: CSS = {
+  '@xx': {},
+}
