7a13be797c42c12c28149898556c5eb847047fb2
Axel Lavoie <axel.lavoie@gmail.com>
2022-11-17 00:32:50 +0100

    fix: style

--- apps/docs-ui/pages/_document.tsx
+++ apps/docs-ui/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from '@pikas-ui/styles';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -51,6 +52,10 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
+          />
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
         </Head>
         <body>
